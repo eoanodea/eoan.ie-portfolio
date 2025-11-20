a0050a6e65a6c7a5267ac46b54ce3ada27c1f091
Eoan O'Dea <odeaeoan@gmail.com>
2025-11-20 20:12:00 +0100

    Updated SEO schema

--- src/components/head.js
+++ src/components/head.js
@@ -9,35 +9,107 @@ import { useStaticQuery, graphql } from 'gatsby';
 const Head = ({ title, description, image }) => {
   const { pathname } = useLocation();
 
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            defaultTitle: title
-            defaultDescription: description
-            siteUrl
-            defaultImage: image
-            twitterUsername
-          }
+  const { site } = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          defaultTitle: title
+          defaultDescription: description
+          siteUrl
+          defaultImage: image
+          twitterUsername
         }
       }
-    `,
-  );
+    }
+  `);
 
-  const {
-    defaultTitle,
-    defaultDescription,
-    siteUrl,
-    defaultImage,
-    twitterUsername,
-  } = site.siteMetadata;
+  const { defaultTitle, defaultDescription, siteUrl, defaultImage, twitterUsername } =
+    site.siteMetadata;
 
   const seo = {
     title: title || defaultTitle,
     description: description || defaultDescription,
     image: `${siteUrl}${image || defaultImage}`,
     url: `${siteUrl}${pathname}`,
+  };
+
+  // Schema.org structured data for Person
+  const personSchema = {
+    '@context': 'https://schema.org',
+    '@type': 'Person',
+    name: 'Eoan O\'Dea',
+    alternateName: 'eoanodea',
+    url: siteUrl,
+    image: `${siteUrl}/me.jpeg`,
+    description:
+      'Irish software engineer, trainer and PhD student in green software and software architecture, focusing on sustainable software systems and energy-efficient microservice architectures.',
+    jobTitle: [
+      'PhD Student in Software Engineering and Green IT',
+      'Software Engineer Trainer',
+      'Managing Director, WebSpace',
+    ],
+    email: 'mailto:hello@eoan.ie',
+    nationality: {
+      '@type': 'Country',
+      name: 'Ireland',
+    },
+    homeLocation: {
+      '@type': 'Place',
+      name: 'L\'Aquila, Italy',
+    },
+    affiliation: [
+      {
+        '@type': 'CollegeOrUniversity',
+        name: 'Università degli Studi dell\'Aquila',
+        sameAs: 'https://www.univaq.it/',
+      },
+      {
+        '@type': 'CollegeOrUniversity',
+        name: 'University of Groningen',
+        sameAs: 'https://www.rug.nl/',
+      },
+      {
+        '@type': 'Organization',
+        name: 'FrAmeLab Software Architecture and Engineering Laboratory',
+        sameAs: 'https://framelab.team',
+      },
+      {
+        '@type': 'Organization',
+        name: 'WebSpace',
+        url: 'https://web-space.ie/',
+      },
+    ],
+    alumniOf: [
+      {
+        '@type': 'CollegeOrUniversity',
+        name: 'Dún Laoghaire Institute of Art, Design and Technology (IADT)',
+        sameAs: 'https://iadt.ie/',
+      },
+    ],
+    knowsAbout: [
+      'Green software',
+      'Green IT',
+      'Software sustainability',
+      'Energy-efficient software systems',
+      'Software architecture',
+      'Microservices',
+      'Web development',
+      'Mobile development',
+      'Flutter',
+      'React',
+      'Vue.js',
+    ],
+    sameAs: [
+      'https://www.linkedin.com/in/eoanodea',
+      'https://github.com/eoanodea',
+      'https://scholar.google.com/citations?user=MSJdQmQAAAAJ',
+      'https://orcid.org/0009-0009-0469-1092',
+      'https://conf.researchr.org/profile/ecsa-2025/eoanodea',
+      'https://conf.researchr.org/profile/ictss-2025/eoanodea',
+      'https://archive.onshow.iadt.ie/2021/student/eoan-odea/',
+      'https://www.researchgate.net/profile/Eoan-Odea',
+      'https://www.framelab.team/rushmore_teams/eoan-odea/',
+    ],
   };
 
   return (
@@ -60,6 +132,9 @@ const Head = ({ title, description, image }) => {
       <meta name="twitter:image" content={seo.image} />
 
       <meta name="google-site-verification" content="DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk" />
+
+      {/* JSON-LD Schema for Person */}
+      <script type="application/ld+json">{JSON.stringify(personSchema)}</script>
     </Helmet>
   );
 };
